3c1e72e31821b41d9e1426b1887a15b736ed7902
wenbin <a030950222@163.com>
2019-03-04 21:53:14 +0800

    add category and update category name function development

--- src/main/java/com/mmall/service/ICategoryService.java
+++ src/main/java/com/mmall/service/ICategoryService.java
@@ -1,8 +1,12 @@
 package com.mmall.service;
 
+import com.mmall.common.ServerResponse;
+
 /**
  * Created by wenbin on 19-2-28.
  */
 public interface ICategoryService {
+    ServerResponse addCategory(String categoryName, Integer parentId);
 
+    ServerResponse updateCategoryName(Integer categoryId, String categoryName);
 }
