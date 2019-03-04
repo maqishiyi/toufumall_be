7f2500c5cac30aa11b0118be6f42613a83867693
wenbin <a030950222@163.com>
2019-03-04 21:28:29 +0800

    user interface finished

---
+++ src/main/java/com/mmall/common/Const.java
@@ -0,0 +1,17 @@
+package com.mmall.common;
+
+/**
+ * Created by wenbin on 19-2-12.
+ */
+public class Const {
+
+    public static final String CURRENT_USER = "currentUser";
+
+    public static final String EMAIL = "email";
+    public static final String USERNAME = "username";
+
+    public interface  Role {
+        int ROLE_CUSTOMER = 0;
+        int ROLE_ADMIN = 1;
+    }
+}
---
+++ src/main/java/com/mmall/service/ICategoryService.java
@@ -0,0 +1,8 @@
+package com.mmall.service;
+
+/**
+ * Created by wenbin on 19-2-28.
+ */
+public interface ICategoryService {
+
+}
