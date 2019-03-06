72fe141b59e64fdc000e0c89f25791c83574219d
wenbin <a030950222@163.com>
2019-03-06 22:18:33 +0800

    get category function development

--- src/main/java/com/mmall/service/ICategoryService.java
+++ src/main/java/com/mmall/service/ICategoryService.java
@@ -1,6 +1,9 @@
 package com.mmall.service;
 
 import com.mmall.common.ServerResponse;
+import com.mmall.pojo.Category;
+
+import java.util.List;
 
 /**
  * Created by wenbin on 19-2-28.
@@ -9,4 +12,8 @@ public interface ICategoryService {
     ServerResponse addCategory(String categoryName, Integer parentId);
 
     ServerResponse updateCategoryName(Integer categoryId, String categoryName);
+
+    ServerResponse<List<Category>> getChildrenParallelCategory(Integer categoryId);
+
+    ServerResponse selectCategoryAndChildrenById(Integer categoryId);
 }
